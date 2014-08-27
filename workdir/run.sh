7016b4446692477b6c5dcd6e662dd89f92f8d5e4
Ali Gurbuz <ali@hazelcast.com>
2014-08-27 11:46:24 +0300

    add provisioner to .gitignore

--- workdir/run.sh
+++ workdir/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-
+#provisioner --scale 10
 
 coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError" \
                 --clientHzFile      ../conf/client-hazelcast.xml \
                 --hzFile            ../conf/hazelcast.xml \
-                --clientWorkerCount 2 \
-                --memberWorkerCount 2 \
+                --clientWorkerCount 0 \
+                --memberWorkerCount 5 \
+                --mixedWorkerCount 0 \
                 --workerClassPath   '../target/*.jar' \
                 --duration          2m \
                 ../conf/test.properties
 
 provisioner --download
 
-
-
+#provisioner --terminate
