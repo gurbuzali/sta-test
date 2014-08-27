ea6b8312d07fb13dcd108ed3f5ba728b91fb5af7
Ali Gurbuz <ali@hazelcast.com>
2014-08-27 12:27:27 +0300

    wip

--- workdir/run.sh
+++ workdir/run.sh
@@ -9,7 +9,7 @@ coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+H
                 --memberWorkerCount 30 \
                 --mixedWorkerCount 0 \
                 --workerClassPath   '../target/*.jar' \
-                --duration          3m \
+                --duration          4m \
                 ../conf/test.properties
 
 provisioner --download
