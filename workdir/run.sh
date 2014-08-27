e18ab979e031040a131d521dc43441502eea9b7b
Ali Gurbuz <ali@hazelcast.com>
2014-08-27 12:09:53 +0300

    latest map-transaction-test

--- workdir/run.sh
+++ workdir/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#provisioner --scale 10
+provisioner --scale 10
 
 coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError" \
                 --clientHzFile      ../conf/client-hazelcast.xml \
                 --hzFile            ../conf/hazelcast.xml \
                 --clientWorkerCount 0 \
-                --memberWorkerCount 5 \
+                --memberWorkerCount 10 \
                 --mixedWorkerCount 0 \
                 --workerClassPath   '../target/*.jar' \
                 --duration          2m \
