bb7ed55751b68e9f7eefdef7354ff5e4046952ca
Ali Gurbuz <ali@hazelcast.com>
2014-09-01 13:05:12 +0300

    wip

--- workdir/run.sh
+++ workdir/run.sh
@@ -5,7 +5,7 @@ provisioner --scale 4
 coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError" \
                 --clientHzFile      ../conf/client-hazelcast.xml \
                 --hzFile            ../conf/hazelcast.xml \
-                --clientWorkerCount 12 \
+                --clientWorkerCount 4 \
                 --memberWorkerCount 4 \
                 --workerClassPath   '../target/*.jar' \
                 --duration          2m \
