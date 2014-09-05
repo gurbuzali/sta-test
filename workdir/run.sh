2c527c2c3dcd93e339280308dcdc58462e2621cd
Ali Gurbuz <ali@hazelcast.com>
2014-09-05 11:41:33 +0300

    wip

--- workdir/run.sh
+++ workdir/run.sh
@@ -4,7 +4,7 @@ provisioner --scale 25
 provisioner --restart
 
 coordinator --memberWorkerCount 25 \
-	--clientWorkerCount 100 \
+	--clientWorkerCount 300 \
 	--duration 10m \
 	--workerClassPath   '../target/*.jar' \
 	--clientHzFile      ../conf/client-hazelcast.xml \
